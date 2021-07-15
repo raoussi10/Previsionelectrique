efb94d2450e0fb7394f78e9dba9dbfced8fcc6a1
Othmane-hub <elkhadiri.othmane@gmail.com>
2021-07-15 18:49:07 +0100

    create_home_page

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,17 +5,11 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
-                    </a>
+                <img src="{{ URL::to('/assets/img/1039.png') }}" style="border-radius:2px; height: 60px; width:100px" >
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-jet-nav-link>
-                </div>
+               
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
